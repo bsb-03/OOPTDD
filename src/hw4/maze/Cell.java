ce49490aa8fa925e9df829fabe95fc2f32642b85
Aiden Harrison <100550195+asharrison97@users.noreply.github.com>
2025-04-22 16:53:30 -0500

    Added getter/setter methods to Cell.java

--- src/hw4/maze/Cell.java
+++ src/hw4/maze/Cell.java
@@ -1,18 +1,51 @@
 package hw4.maze;
 
 public class Cell {
-	CellComponents up, bottom, left, right;
+	CellComponents up, down, left, right;
 	public Cell() {
 		up = null;
-		bottom = null;
+		down = null;
 		left = null;
 		right = null;
 	}
 	
 	public Cell(CellComponents u, CellComponents b, CellComponents l, CellComponents r) {
 		up = u;
-		bottom = b;
+		down = b;
 		left = l;
 		right = r;
 	}
+
+	public CellComponents getUp() {
+		return up;
+	}
+
+	public void setUp(CellComponents up) {
+		this.up = up;
+	}
+
+	public CellComponents getDown() {
+		return down;
+	}
+
+	public void setDown(CellComponents bottom) {
+		this.down = bottom;
+	}
+
+	public CellComponents getLeft() {
+		return left;
+	}
+
+	public void setLeft(CellComponents left) {
+		this.left = left;
+	}
+
+	public CellComponents getRight() {
+		return right;
+	}
+
+	public void setRight(CellComponents right) {
+		this.right = right;
+	}
+	
 }
