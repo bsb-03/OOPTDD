e6715f716f6112b7a00932eacbf7acd419f18f22
Aiden Harrison <100550195+asharrison97@users.noreply.github.com>
2025-04-23 20:20:24 -0500

    Added javadoc code to Row.java

--- src/hw4/maze/Row.java
+++ src/hw4/maze/Row.java
@@ -5,23 +5,42 @@ import java.util.ArrayList;
 public class Row {
 	private ArrayList<Cell> cells;
 	
+	/**
+	 * toString() method for Row class
+	 */
 	@Override
 	public String toString() {
 		return "Row [cells=" + cells + "]";
 	}
-
+	
+	/**
+	 * Default constructor for Row class
+	 */
 	public Row() {
 		cells = null;
 	}
 	
+	/**
+	 * Parameterized constructor for Row class. Takes
+	 * an ArrayList of cells and assigns it to cells field
+	 * @param c - ArrayList of cells
+	 */
 	public Row(ArrayList<Cell> c) {
 		cells = c;
 	}
 	
+	/**
+	 * Getter method for Cells class
+	 * @return
+	 */
 	public ArrayList<Cell> getCells() {
 		return cells;
 	}
 	
+	/**
+	 * Setter method for Cells class
+	 * @param c
+	 */
 	public void setCells(ArrayList<Cell> c) {
 		this.cells = c;
 	}
