47e7f851dc157b58bffa78dfc59e73554e788cc7
Aiden Harrison <100550195+asharrison97@users.noreply.github.com>
2025-04-23 20:23:56 -0500

    Added javadoc code to Player.java file

--- src/hw4/player/Player.java
+++ src/hw4/player/Player.java
@@ -10,6 +10,9 @@ public class Player {
 	private int currentRowInt;
 	private int currentCellInt;
 	
+	/**
+	 * Default constructor for Player class
+	 */
 	public Player() {
 		currentRow = null;
 		currentCell = null;
@@ -18,6 +21,14 @@ public class Player {
 		currentCellInt = -1;
 	}
 	
+	/**
+	 * Parameterized constructor for Player class. Takes a row and cell object
+	 * as parameters, which represent the position of the player object which is the
+	 * agent within the game.
+	 * 
+	 * @param r - Row object to populate row field
+	 * @param c - Cell object to populate cell field
+	 */
 	public Player(Row r, Cell c) {
 		currentRow = r;
 		currentCell = c;
@@ -26,40 +37,75 @@ public class Player {
 		currentCellInt = -1;
 	}
 	
+	/**
+	 * Getter method for currentRowInt field
+	 * @return
+	 */
 	public int getCurrentRowInt() {
 		return currentRowInt;
 	}
 	
+	/**
+	 * Setter method for currentRowInt field
+	 * @param r
+	 */
 	public void setCurrentRowInt(int r) {
 		this.currentRowInt = r;
 	}
 	
+	/**
+	 * Getter method for currentCellInt field
+	 * @return
+	 */
 	public int getCurrentCellInt() {
 		return currentCellInt;
 	}
 	
+	/**
+	 * Setter method for currentCellInt field
+	 * @param c
+	 */
 	public void setCurrentCellInt(int c) {
 		this.currentCellInt = c;
 	}
-
+	
+	/**
+	 * Getter method for currentRow field
+	 * @return
+	 */
 	public Row getCurrentRow() {
 		return currentRow;
 	}
-
-	@Override
-	public String toString() {
-		return "Player [currentCell=" + currentCell + ", currentRow=" + currentRow + "]";
-	}
-
+	
+	/**
+	 * Setter method for currentRow field
+	 * @param currentRow
+	 */
 	public void setCurrentRow(Row currentRow) {
 		this.currentRow = currentRow;
 	}
-
+	
+	/**
+	 * Getter method for currentCell field
+	 * @return
+	 */
 	public Cell getCurrentCell() {
 		return currentCell;
 	}
-
+	
+	/**
+	 * Setter method for currentCell field
+	 * @param currentCell
+	 */
 	public void setCurrentCell(Cell currentCell) {
 		this.currentCell = currentCell;
+	}
+	
+	/**
+	 * toString() method for Player class
+	 */
+	@Override
+	public String toString() {
+		return "Player [currentCell=" + currentCell + ", currentRow=" + currentRow + "]";
 	}
 }
