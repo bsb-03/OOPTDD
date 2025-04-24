ed38767840285add84e0d010381ad0eb4578f8b3
Aiden Harrison <100550195+asharrison97@users.noreply.github.com>
2025-04-23 20:18:50 -0500

    Added javadoc code to Grid.java

--- src/hw4/maze/Grid.java
+++ src/hw4/maze/Grid.java
@@ -5,23 +5,42 @@ import java.util.ArrayList;
 public class Grid {
 	private ArrayList<Row> rows;
 	
+	/**
+	 * toString() method for Grid class
+	 */
 	@Override
 	public String toString() {
 		return "Grid [rows=" + rows + "]";
 	}
-
+	
+	/**
+	 * Default constructor for Grid class
+	 */
 	public Grid() {
 		rows = null;
 	}
 	
+	/**
+	 * Parameterized constructor for Grid class. Takes an ArrayList of rows
+	 * as a parameter to assign to rows field
+	 * @param r - ArrayList of rows
+	 */
 	public Grid(ArrayList<Row> r) {
 		rows = r;
 	}
 	
+	/**
+	 * Getter method for rows field
+	 * @return
+	 */
 	public ArrayList<Row> getRows() {
 		return rows;
 	}
 	
+	/**
+	 * Setter method for rows field
+	 * @param r
+	 */
 	public void setRows(ArrayList<Row> r) {
 		this.rows = r;
 	}
