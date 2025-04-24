a4076ffb8c69bfd30902bac7be508faad79c2af8
Aiden Harrison <100550195+asharrison97@users.noreply.github.com>
2025-04-23 20:15:54 -0500

    Added javadoc code to Cell.java

--- src/hw4/maze/Cell.java
+++ src/hw4/maze/Cell.java
@@ -2,6 +2,10 @@ package hw4.maze;
 
 public class Cell {
 	private CellComponents up, down, left, right;
+	
+	/**
+	 * Default constructor for Cell class.
+	 */
 	public Cell() {
 		up = null;
 		down = null;
@@ -9,11 +13,13 @@ public class Cell {
 		right = null;
 	}
 	
-	@Override
-	public String toString() {
-		return "Cell [left=" + left + ", right=" + right + ", up=" + up + ", down=" + down + "]";
-	}
-
+	/**
+	 * Parameterized constructor for Cell class.
+	 * @param u - CellComponents values for left part of cell.
+	 * @param b - CellComponents values for right part of cell.
+	 * @param l - CellComponents values for top part of cell.
+	 * @param r - CellComponents values for bottom part of cell.
+	 */
 	public Cell(CellComponents u, CellComponents b, CellComponents l, CellComponents r) {
 		left = u;
 		right = b;
@@ -21,34 +27,74 @@ public class Cell {
 		down = r;
 	}
 
+	/**
+	 * toString() method for Cell class.
+	 */
+	@Override
+	public String toString() {
+		return "Cell [left=" + left + ", right=" + right + ", up=" + up + ", down=" + down + "]";
+	}
+	
+	/**
+	 * Getter method for up field
+	 * @return
+	 */
 	public CellComponents getUp() {
 		return up;
 	}
-
+	
+	/**
+	 * Setter method for up field
+	 * @param up
+	 */
 	public void setUp(CellComponents up) {
 		this.up = up;
 	}
-
+	
+	/**
+	 * Getter method for down field
+	 * @return
+	 */
 	public CellComponents getDown() {
 		return down;
 	}
-
+	
+	/**
+	 * Setter method for down field
+	 * @param bottom
+	 */
 	public void setDown(CellComponents bottom) {
 		this.down = bottom;
 	}
-
+	
+	/**
+	 * Getter method for left field
+	 * @return
+	 */
 	public CellComponents getLeft() {
 		return left;
 	}
-
+	
+	/**
+	 * Setter method for left field
+	 * @param left
+	 */
 	public void setLeft(CellComponents left) {
 		this.left = left;
 	}
-
+	
+	/**
+	 * Getter method for right field
+	 * @return
+	 */
 	public CellComponents getRight() {
 		return right;
 	}
 
+	/**
+	 * Setter method for right field
+	 * @param right
+	 */
 	public void setRight(CellComponents right) {
 		this.right = right;
 	}
